9c99cd203e10f6186a8f7a4ff72ee5b80cedf0a0
Olivier Boucher <olivier@bouchernolet.com>
2015-11-15 18:04:34 -0500

    Parser exception

--- src/java/com/olivierboucher/inf1018/syntactical/ParserException.java
+++ src/java/com/olivierboucher/inf1018/syntactical/ParserException.java
@@ -3,5 +3,8 @@ package com.olivierboucher.inf1018.syntactical;
 /**
  * Created by olivier on 2015-11-15.
  */
-public class ParserException {
+public class ParserException extends Exception {
+    public ParserException(String msg){
+        super(msg);
+    }
 }
